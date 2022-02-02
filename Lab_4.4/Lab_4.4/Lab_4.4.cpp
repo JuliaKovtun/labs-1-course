e4e72f708e65f9d9eaf01af6e085d9b6294fdc5f
JuliaKovtun <julia141407@gmail.com>
2022-02-02 23:25:40 +0200

    C++, first semester

---
+++ Lab_4.4/Lab_4.4/Lab_4.4.cpp
@@ -0,0 +1,64 @@
+﻿
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+
+	double xp, xk, dx;
+	double x, R1, R2, y;
+
+	cout << "R1 = "; cin >> R1;
+	cout << "R2 = "; cin >> R2;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+
+	cout << fixed;
+	cout << "---------------------" << endl;
+	cout << "|" << setw(5) << "x" << setw(5) << "|"
+		<< setw(5) << "y" << setw(5) << "|" << endl
+		<< "---------------------" << endl;
+
+	x = xp;
+
+	while (x <= xk)
+	{
+
+		if (x <= -6)
+			y = R2 / 2;
+		else
+			if (x > -6 && x <= -2 * R2)
+				y = (-R2 * (x + 4 * R2)) / (4 * (3 - R2));
+			else
+				if (x < -2 * R2 && x <= 0)
+					y = sqrt(R2 * R2 - pow((x + 2), 2));
+				else
+					if (x >= 0 && x <= 2 * R1)
+						y = -sqrt(R1 * R1 - pow((x - 1), 2));
+					else
+						y = R1 * x - 2 * R1 * R1;
+
+		cout << "|" << setw(8) << setprecision(2) << x
+			<< " |" << setw(8) << setprecision(2) << y
+			<< " |" << endl;
+
+		x += dx;
+
+
+	}
+
+	cout << "---------------------" << endl;
+
+	return 0;
+
+
+
+
+
+}
+
---
+++ Lab_4.6/Lab_4.6/Lab_4.6.cpp
@@ -0,0 +1,93 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+ int N = 15, i = 1, k;
+ double S = 0, P;
+
+ while(i <= N)
+ { 
+	 P = 1;
+	 k = 1;
+
+	 while (k <= i)
+	 {
+		 P *= sqrt(k);
+		 k++;
+	 }
+	
+	 S += (sin(10 * i) + cos(10 * i)) / P;
+	 i++;
+ }
+
+ cout << S << endl;
+
+ S = 0;
+ i = 1;
+
+ do 
+ {
+	 P = 1;
+	 k = 1;
+
+	 do 
+	 {
+		 P *= sqrt(k);
+		 k++;
+	 } while (k <= i);
+
+	 S += (sin(10 * i) + cos(10 * i)) / P;
+	 i++;
+
+ } while (i <= N);
+
+ cout << S << endl;
+
+
+ S = 0;
+ i = 1;
+
+ for (i; i <= N; i++)
+ {
+	 P = 1;
+	 k = 1;
+
+	 for (k; k <= i; k++)
+	 {
+		 P *= sqrt(k);
+	
+	 }
+
+	 S += (sin(10 * i) + cos(10 * i)) / P;
+	
+
+ }
+
+ cout << S << endl;
+
+ S = 0;
+ i = N;
+
+ for (i; i >= 1; i--)
+ {
+	 P = 1;
+
+	 for (k = i; k >= 1; k--)
+	 {
+		 P *= sqrt(k);
+
+	 }
+	 S += (sin(10 * i) + cos(10 * i)) / P;
+
+
+ }
+
+ cout << S << endl;
+
+
+}
+
